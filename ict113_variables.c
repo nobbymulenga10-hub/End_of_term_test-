1b42ff7b66e5e81ebe6b790e82403c9b0d553754
Sinyangwe370530 <troyreigns6@gmail.com>
2025-12-26 18:44:10 +0200

    Initial commit

---
+++ ict113_variables.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+    char name[] = "Mulenga Nobby";
+    int age = 25;
+    float gpa = 4.5;
+
+
+    printf("Name: ");
+     printf("%s\n", name);
+
+    printf("Age: ");
+      printf("%d\n", age);
+
+    printf("GPA: ");
+      printf("%.1f\n", gpa);
+
+
+    printf("----------------\n");
+     printf("End of program\n");
+
+    return 0;
+}
